1790dfa83a077a6eeaa8531d55ddc10ce76faae9
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-17 22:48:56 +0600

    add Phaser

--- src/game/scenes/game.scene.ts
+++ src/game/scenes/game.scene.ts
@@ -1,3 +1,32 @@
+import * as Phaser from "phaser"
+
 export function useGameScene() {
-    console.log("Start game!")
+    const gameContainer = <HTMLDivElement>document.getElementById("game")
+
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        parent: gameContainer,
+        physics: {
+            default: "arcade",
+            arcade: {
+                gravity: { y: 300 },
+                debug: false,
+            },
+        },
+        scene: {
+            preload,
+            create,
+            update,
+        },
+    }
+
+    function preload() {}
+
+    function create() {}
+
+    function update() {}
+
+    return new Phaser.Game(config)
 }
