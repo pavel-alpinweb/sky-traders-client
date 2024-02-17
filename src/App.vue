65011757813e396898f40927aeb32b7208053109
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-17 21:16:04 +0600

    create game screen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import StartScreen from "./components/screens/Start.screen.vue"
+import GameScreen from "./components/screens/Game.screen.vue"
 </script>
 
 <template>
     <div class="app">
+        <GameScreen />
         <StartScreen />
     </div>
 </template>
