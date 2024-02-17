91a577ab373e8ab4924a7921353a8284aa7ec187
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-17 20:15:40 +0600

    add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from "vue"
 
-const title = ref('Age of Sky Traders');
+const title = ref("Age of Sky Traders")
 </script>
 
 <template>
-  <div class="app">
-    <h1>{{ title }}</h1>
-  </div>
+    <div class="app">
+        <h1>{{ title }}</h1>
+    </div>
 </template>
 
 <style lang="scss">
 $color: #fff;
 
 h1 {
-  color: $color;
+    color: $color;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      vueTsc: true,
+      eslint: {
+        // for example, lint .ts and .tsx
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
 })
