f07c6eac4e90a8cac9409246f3d2b8e7bea17ea1
Павел Бездорнов <alpinweb@yandex.ru>
2024-02-24 10:53:36 +0600

    create test route

---
+++ src/components/screens/Town.screen.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Test from "../ui-components/Test.component.vue"
+</script>
+
+<template>
+    <div class="town-screen">
+        <h1>Town screen</h1>
+        <Test />
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
