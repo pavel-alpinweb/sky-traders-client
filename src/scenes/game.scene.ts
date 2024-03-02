ad088bfeb8ebdcc54ec4c0f94d8a34504491c982
Павел Бездорнов <alpinweb@yandex.ru>
2024-03-02 14:06:06 +0600

    create playerComposition & fix mapBackgroundComposition

--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -1,6 +1,6 @@
 import * as Phaser from "phaser"
-import { BASIC_SHIP_ANGULAR_VELOCITY, BASIC_SHIP_ROTATION_VELOCITY, BASIC_SHIP_SCALE, BASIC_SHIP_SPEED, TARGET_TOLERANCE } from "../configs/gameplay.config.ts"
-import { mapAssetsUploadComposition } from "../compositions/map-assets-upload.composition.ts"
+import { mapBackgroundComposition } from "../compositions/map-background.composition.ts"
+import { playerComposition } from "../compositions/player.composition.ts"
 
 export class MapScene extends Phaser.Scene {
     private player!: Phaser.Physics.Arcade.Image & { body: Phaser.Physics.Arcade.Body }
@@ -10,41 +10,17 @@ export class MapScene extends Phaser.Scene {
     }
 
     preload() {
-        mapAssetsUploadComposition.playerShipUpload(this, "ship")
-        mapAssetsUploadComposition.mapBackgroundUpload(this)
+        mapBackgroundComposition.playerShipUpload(this, "ship")
+        mapBackgroundComposition.mapBackgroundUpload(this)
     }
 
     create() {
-        this.add.image(window.innerWidth / 2, window.innerHeight / 2, "sky")
-        this.player = this.physics.add
-            .image(window.innerWidth / 2, window.innerHeight / 2, "ship")
-            .setScale(BASIC_SHIP_SCALE)
-            .refreshBody()
-        this.target = new Phaser.Math.Vector2()
-        this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
-            this.target.x = pointer.x
-            this.target.y = pointer.y
-
-            this.physics.moveToObject(this.player, this.target, BASIC_SHIP_SPEED)
-            this.player.setAngularVelocity(BASIC_SHIP_ANGULAR_VELOCITY).refreshBody()
-        })
+        mapBackgroundComposition.addMapBackground(this)
+        this.player = playerComposition.initPlayer(this, "ship", window.innerWidth / 2, window.innerHeight / 2)
+        this.target = playerComposition.movePlayer(this, this.player)
     }
 
     update() {
-        const angleToPointer = Phaser.Math.RadToDeg(Phaser.Math.Angle.Between(this.player.x, this.player.y, this.target.x, this.target.y))
-        const angleDelta = Phaser.Math.Angle.ShortestBetween(this.player.body.rotation, angleToPointer)
-        const distance = Phaser.Math.Distance.BetweenPoints(this.player, this.target)
-
-        if (Phaser.Math.Fuzzy.Equal(angleDelta, 0, TARGET_TOLERANCE)) {
-            this.player.body.rotation = angleToPointer
-            this.player.setAngularVelocity(0)
-        } else {
-            this.player.setAngularVelocity(Math.sign(angleDelta) * BASIC_SHIP_ANGULAR_VELOCITY)
-        }
-
-        if (distance < TARGET_TOLERANCE) {
-            this.player.body.reset(this.target.x, this.target.y)
-            this.physics.velocityFromRotation(Phaser.Math.DegToRad(this.player.body.rotation), BASIC_SHIP_ROTATION_VELOCITY, this.player.body.velocity)
-        }
+        playerComposition.rotatePlayer(this, this.player, this.target)
     }
 }
