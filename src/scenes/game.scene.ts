2e47c1383cb923c7b9f81ed0483454eea26d2fa1
Павел Бездорнов <alpinweb@yandex.ru>
2024-05-17 10:09:21 +0600

    remove no use methods

--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -5,12 +5,6 @@ import { playerComposition } from "../compositions/player.composition.ts"
 export class MapScene extends Phaser.Scene {
     private player!: Phaser.Physics.Arcade.Image & { body: Phaser.Physics.Arcade.Body }
     private target!: Phaser.Math.Vector2
-    private town!: Phaser.Types.Physics.Arcade.SpriteWithStaticBody
-    private handler!: () => void
-    constructor(callback: () => void) {
-        super()
-        this.handler = callback
-    }
 
     preload() {
         mapComposition.playerShipUpload(this, "ship")
@@ -19,17 +13,11 @@ export class MapScene extends Phaser.Scene {
 
     create() {
         mapComposition.addMapBackground(this)
-        this.town = mapComposition.addMapTown(this, "town")
         this.player = playerComposition.initPlayer(this, "ship", window.innerWidth / 2, window.innerHeight / 2)
         this.target = playerComposition.movePlayer(this, this.player)
-        this.physics.add.overlap(this.player, this.town, this.goToTown, undefined, this)
     }
 
     update() {
         playerComposition.rotatePlayer(this, this.player, this.target)
-    }
-
-    goToTown() {
-        this.handler()
     }
 }
