d9a5d5b4d21fbd7a9b3b08ebb3f549cd6d7b6e58
Павел Бездорнов <alpinweb@yandex.ru>
2024-05-20 10:07:16 +0600

    add surface tile

--- src/compositions/map.composition.ts
+++ src/compositions/map.composition.ts
@@ -1,4 +1,5 @@
 import Phaser from "phaser"
+import { LEVEL_HEIGHT, LEVEL_WIDTH } from "../configs/gameplay.config.ts"
 
 export const mapComposition = {
     playerShipUpload(scene: Phaser.Scene, ship: string): void {
@@ -6,11 +7,11 @@ export const mapComposition = {
     },
 
     mapBackgroundUpload(scene: Phaser.Scene): void {
-        scene.load.image("map", "/assets/backgrounds/map3.jpg")
+        scene.load.image("map", "/assets/tiles/surface.png")
     },
 
     addMapBackground(scene: Phaser.Scene): void {
-        scene.add.image(window.innerWidth / 2, window.innerHeight / 2, "map")
+        scene.add.tileSprite(LEVEL_WIDTH / 2, LEVEL_HEIGHT / 2, LEVEL_WIDTH, LEVEL_HEIGHT, "map").setScrollFactor(0.5)
     },
 
     addMapTown(scene: Phaser.Scene, key: string): Phaser.Types.Physics.Arcade.SpriteWithStaticBody {
