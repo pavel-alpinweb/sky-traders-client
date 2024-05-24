3843802aa1beec3d838139cefef501290d0781dc
Павел Бездорнов <alpinweb@yandex.ru>
2024-05-24 21:14:29 +0600

    create module town

---
+++ src/store/town.ts
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia"
+
+interface Coords {
+    x: number
+    y: number
+}
+
+interface Town {
+    name: string
+    coords: Coords
+}
+
+export const Town = defineStore("town", {
+    state: (): Town => ({
+        name: "",
+        coords: {
+            x: 0,
+            y: 0,
+        },
+    }),
+    actions: {
+        setTown: (name: string): void => {
+            // eslint-disable-next-line
+            // @ts-ignore
+            this.name = name
+        },
+        setCoords: (coords: Coords): void => {
+            // eslint-disable-next-line
+            // @ts-ignore
+            this.coords = coords
+        },
+    },
+})
