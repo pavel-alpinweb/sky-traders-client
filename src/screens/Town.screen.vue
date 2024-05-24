ad59b10569088ce822d9d74c66e340cae760e78b
Павел Бездорнов <alpinweb@yandex.ru>
2024-05-24 22:33:05 +0600

    comeback to map screen

--- src/screens/Town.screen.vue
+++ src/screens/Town.screen.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import Test from "../ui-components/Test.component.vue"
+import { router } from "../router.ts"
+
+const goToMap = () => {
+    router.push({ path: "/game" })
+}
 </script>
 
 <template>
     <div class="town-screen">
         <h1>Town screen</h1>
-        <button>Покинуть город</button>
+        <button @click="goToMap">Покинуть город</button>
         <Test />
     </div>
 </template>
