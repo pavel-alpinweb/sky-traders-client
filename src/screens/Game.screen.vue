a9c20ed5668e7f9c9f47e15e648f01ed3d065e92
Павел Бездорнов <alpinweb@yandex.ru>
2024-05-24 11:58:01 +0600

    show alert on flying on town

--- src/screens/Game.screen.vue
+++ src/screens/Game.screen.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
-import { onMounted } from "vue"
+import { onMounted, ref } from "vue"
 import { useMapLevel } from "../levels/map.level.ts"
+import { EventBus } from "../utils/EventBus.ts"
+
+const townName = ref("")
+const isShowTownAlert = ref(false)
 
 onMounted(() => {
     useMapLevel()
+    // eslint-disable-next-line
+    EventBus.on("fly-on-town", (params: any) => {
+        townName.value = params.town
+        isShowTownAlert.value = true
+    })
+    EventBus.on("live-town", () => {
+        isShowTownAlert.value = false
+    })
 })
 </script>
 
 <template>
-    <div id="game" class="game-screen"></div>
+    <div class="game-screen">
+        <div v-if="isShowTownAlert" class="game-screen__town-alert">
+            <div class="game-screen__name">Вы хотите приземлиться в городе {{ townName }}?</div>
+            <button>В город!</button>
+        </div>
+        <div id="game" class="game-screen__game-wrapper"></div>
+    </div>
 </template>
 
 <style scoped lang="scss">
@@ -19,9 +37,20 @@ onMounted(() => {
     display: flex;
     align-items: center;
     justify-content: center;
-    canvas {
-        width: 100%;
-        height: 100%;
+    position: relative;
+    &__game-wrapper {
+        width: 1365px;
+        height: 768px;
+    }
+    &__town-alert {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 200px;
+        height: 50px;
+        background-color: #fff;
+        color: #1a1a1a;
     }
 }
 </style>
