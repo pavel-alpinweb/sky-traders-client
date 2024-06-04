09e716bd151286c4da86a07c6703ca3e0b157632
Павел Бездорнов <alpinweb@yandex.ru>
2024-06-04 11:30:39 +0600

    create town level scene

---
+++ src/scenes/town.scene.ts
@@ -0,0 +1,16 @@
+import * as Phaser from "phaser"
+
+export class TownScene extends Phaser.Scene {
+    private name!: string
+
+    constructor(name: string) {
+        super()
+        this.name = name
+    }
+
+    preload() {}
+
+    create() {}
+
+    update() {}
+}
