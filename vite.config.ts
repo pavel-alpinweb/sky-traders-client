827cfcc4a01486f0866f2b51ae3f4701ed41cd19
Павел Бездорнов <alpinweb@yandex.ru>
2024-06-05 21:14:47 +0600

    Добавил иконки в проект

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import checker from 'vite-plugin-checker';
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import checker from "vite-plugin-checker"
+import svgLoader from "vite-svg-loader"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    checker({
-      vueTsc: true,
-      eslint: {
-        // for example, lint .ts and .tsx
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-      },
-    }),
-  ],
+    plugins: [
+        vue(),
+        svgLoader(),
+        checker({
+            vueTsc: true,
+            eslint: {
+                // for example, lint .ts and .tsx
+                lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+            },
+        }),
+    ],
 })
