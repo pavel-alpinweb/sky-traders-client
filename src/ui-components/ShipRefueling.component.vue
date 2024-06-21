0e857e58a273a3b00a5a54834586eddf50d11d05
Павел Бездорнов <alpinweb@yandex.ru>
2024-06-21 10:51:58 +0600

    create ShipRefueling.component.vue

---
+++ src/ui-components/ShipRefueling.component.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, ref } from "vue"
+import { FUEL_PRICE } from "../configs/gameplay.config.ts"
+
+const props = defineProps<{
+    maxFuel: number
+    currentFuel: number
+}>()
+
+const fuelAmount = ref<number>(props.currentFuel)
+
+const fuelBill = computed(() => fuelAmount.value * FUEL_PRICE)
+</script>
+
+<template>
+    <div class="ship-refueling"></div>
+</template>
+
+<style scoped lang="scss"></style>
