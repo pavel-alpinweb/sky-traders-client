9bee6eea223956d505844133f5ca829520f50004
Павел Бездорнов <alpinweb@yandex.ru>
2024-07-01 11:57:48 +0600

    set player start ship

--- src/store/player.ts
+++ src/store/player.ts
@@ -61,7 +61,7 @@ export const usePlayer = defineStore("player", {
                 repairPrice: 8,
             },
         ],
-        currentShipId: 1,
+        currentShipId: 6,
     }),
     getters: {
         currentShip: (state): Ship => {
