611be927c81370be88ffa081d1f6d27c72f04ba5
Павел Бездорнов <alpinweb@yandex.ru>
2024-07-02 11:55:40 +0600

    setup pirate fire

--- src/configs/gameplay.config.ts
+++ src/configs/gameplay.config.ts
@@ -18,9 +18,9 @@ export const FIRE_BUTTON = "space"
 export const BULLET_VELOCITY = 1000
 export const PIRATE_VELOCITY = 150
 export const PIRATE_STOP_TOLERANCE = 500
-export const PIRATE_FIRE_DELAY = 700
-export const PIRATE_START_FIRE_DELAY = 700
-export const PIRATE_FIRE_DISTANCE = 700
+export const PIRATE_FIRE_DELAY = 2500
+export const PIRATE_START_FIRE_DELAY = 2500
+export const PIRATE_FIRE_DISTANCE = 800
 
 /* Town Settings */
 export const TOWN_SIZE = 1024
