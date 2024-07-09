18bb6814d90a1b14b303f9c5ce399405406347e6
Павел Бездорнов <alpinweb@yandex.ru>
2024-07-09 11:31:01 +0600

    upload spawner image

--- src/compositions/pirates.composition.ts
+++ src/compositions/pirates.composition.ts
@@ -3,5 +3,6 @@ import Phaser from "phaser"
 export const piratesComposition = {
     piratesShipsUpload(scene: Phaser.Scene) {
         scene.load.image("shark", "/assets/ships/shark/shark-map.png")
+        scene.load.image("spawner", "/assets/tiles/spawner.jpg")
     },
 }
