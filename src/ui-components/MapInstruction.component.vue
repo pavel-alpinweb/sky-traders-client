bf57b667aa77c77190d9f02a01d603c75a04e6bc
Павел Бездорнов <alpinweb@yandex.ru>
2024-08-16 11:52:41 +0600

    change icons

--- src/ui-components/MapInstruction.component.vue
+++ src/ui-components/MapInstruction.component.vue
@@ -4,6 +4,9 @@ import IconFuel from "/public/assets/icons/ship-params/fuel.svg"
 import IconArmor from "/public/assets/icons/ship-params/shield.svg"
 import IconSink from "/public/assets/icons/alerts/sink.svg"
 import IconPirate from "/public/assets/icons/alerts/pirate.svg"
+import IconTown from "/public/assets/icons/map/town.svg"
+import IconShips from "/public/assets/icons/shipyard/zeppelin.svg"
+import IconAward from "/public/assets/icons//alerts/award.svg"
 
 const props = defineProps<{
     color: string
@@ -19,33 +22,32 @@ const props = defineProps<{
             <v-card title="Инструкции во время полетов" :color="`${props.color}-lighten-5`">
                 <v-card-text>
                     Для передвижения по небу нажмите левой кнопкой мыши <v-icon icon="mdi-mouse-left-click" :color="props.color" /> на место, куда вы хотите полететь, и ваш корабль
-                    <v-icon icon="mdi-airballoon" :color="props.color" /> начнет движение <v-icon icon="mdi-ship-wheel" :color="props.color" /> в этом направлении.
+                    <IconShips class="instruction-icon" /> начнет движение <v-icon icon="mdi-ship-wheel" :color="props.color" /> в этом направлении.
                 </v-card-text>
                 <v-card-text>Для ориентации используйте карту <v-icon icon="mdi-map" :color="props.color" />, которую можно вызвать в нижнем левом углу.</v-card-text>
                 <v-card-text
-                    >Во время передвижения <v-icon icon="mdi-ship-wheel" :color="props.color" /> ваш корабль <v-icon icon="mdi-airballoon" :color="props.color" /> тратит топливо
+                    >Во время передвижения <v-icon icon="mdi-ship-wheel" :color="props.color" /> ваш корабль <IconShips class="instruction-icon" /> тратит топливо
                     <IconFuel class="instruction-icon" />. Также состояние <IconArmor class="instruction-icon" /> корабля медленно ухудшается.</v-card-text
                 >
                 <v-card-text
                     >Если хотя бы один показатель корабля <IconArmor class="instruction-icon" /> <IconFuel class="instruction-icon" /> опустится до нуля, ваш корабль потерпит крушение
-                    <IconSink class="instruction-icon" />. В этом случае вы потеряете корабль <v-icon icon="mdi-airballoon" :color="props.color" /> и весь ваш груз
+                    <IconSink class="instruction-icon" />. В этом случае вы потеряете корабль <IconShips class="instruction-icon" /> и весь ваш груз
                     <v-icon icon="mdi-train-car-container" :color="props.color" />.</v-card-text
                 >
                 <v-card-text>Золото <IconGold class="instruction-icon" /> при крушении <IconSink class="instruction-icon" /> не теряется.</v-card-text>
                 <v-card-text>Ваш груз <v-icon icon="mdi-train-car-container" :color="props.color" /> показан на панели сверху.</v-card-text>
                 <v-card-text
                     >В некоторых районах неба на вас могут напасть пираты <IconPirate class="instruction-icon" />. Цель пиратов <IconPirate class="instruction-icon" /> — уничтожить ваш корабль
-                    <v-icon icon="mdi-airballoon" :color="props.color" /> и захватить груз <v-icon icon="mdi-train-car-container" :color="props.color" />.</v-card-text
+                    <IconShips class="instruction-icon" /> и захватить груз <v-icon icon="mdi-train-car-container" :color="props.color" />.</v-card-text
                 >
-                <v-card-text
-                    >Чтобы скрыться от пиратов <IconPirate class="instruction-icon" />, вы должны приземлиться в любом из городов <v-icon icon="mdi-home-city" :color="props.color" />.</v-card-text
-                >
+                <v-card-text>Чтобы скрыться от пиратов <IconPirate class="instruction-icon" />, вы должны приземлиться в любом из городов <IconTown class="instruction-icon" />.</v-card-text>
                 <v-card-text
                     >Вы можете сразиться с пиратами <IconPirate class="instruction-icon" />. Для того чтобы стрелять по пиратам, нажмите клавишу "ПРОБЕЛ"
                     <v-icon icon="mdi-keyboard-space" :color="props.color" />.</v-card-text
                 >
                 <v-card-text class="instruction"
-                    >За уничтожение каждого пиратского корабля <IconPirate class="instruction-icon" /> вы получаете награду в 10000 <IconGold class="instruction-icon"
+                    >За уничтожение <IconAward class="instruction-icon" /> каждого пиратского корабля <IconPirate class="instruction-icon" /> вы получаете награду в 10000
+                    <IconGold class="instruction-icon"
                 /></v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
--- src/ui-components/TownInstraction.component.vue
+++ src/ui-components/TownInstraction.component.vue
@@ -6,6 +6,9 @@ import IconShipyard from "/public/assets/icons/screens/shipyard.svg"
 import IconFuel from "/public/assets/icons/ship-params/fuel.svg"
 import IconArmor from "/public/assets/icons/ship-params/shield.svg"
 import IconPirate from "/public/assets/icons/alerts/pirate.svg"
+import IconShips from "/public/assets/icons/shipyard/zeppelin.svg"
+import IconBuy from "/public/assets/icons/shipyard/buy.svg"
+import IconTown from "/public/assets/icons/map/town.svg"
 
 const props = defineProps<{
     color: string
@@ -19,47 +22,42 @@ const props = defineProps<{
         </template>
         <template #default="{ isActive }">
             <v-card title="Инструкции для городов" :color="`${props.color}-lighten-5`">
-                <v-card-text
-                    >Главная цель вашего пребывания в городе <v-icon icon="mdi-home-city" :color="props.color" /> — это торговля
-                    <v-icon icon="mdi-plus-circle-multiple" :color="props.color" />.</v-card-text
-                >
-                <v-card-text> Торговля осуществляется на экране рынка <IconMarket class="instruction-icon" />. </v-card-text>
+                <v-card-text>Главная цель вашего пребывания в городе <IconTown class="instruction-icon" /> — это торговля <v-icon icon="mdi-plus-circle-multiple" :color="props.color" />.</v-card-text>
+                <v-card-text> Торговля осуществляется на экране рынка <IconMarket class="instruction-icon" /> </v-card-text>
                 <v-card-text>
-                    Спекулируя на ценах <v-icon icon="mdi-label-percent-outline" :color="props.color" />, покупая <v-icon icon="mdi-minus-circle-multiple" :color="props.color" /> одни товары
+                    Спекулируя на ценах <IconGold class="instruction-icon" />, покупая <v-icon icon="mdi-minus-circle-multiple" :color="props.color" /> одни товары
                     <IconSilk class="instruction-icon instruction-icon--silk" /> дешевле и продавая <v-icon icon="mdi-plus-circle-multiple" :color="props.color" /> их дороже , вы зарабатываете золото
                     <IconGold class="instruction-icon" />
                 </v-card-text>
-                <v-card-text
-                    >Каждый город <v-icon icon="mdi-home-city" :color="props.color" /> специализируется на разных товарах <IconSilk class="instruction-icon instruction-icon--silk" />.
+                <v-card-text>Каждый город <IconTown class="instruction-icon" /> специализируется на разных товарах <IconSilk class="instruction-icon instruction-icon--silk" />. </v-card-text>
+                <v-card-text>
+                    Одни и те же товары <IconSilk class="instruction-icon instruction-icon--silk" /> в разных городах <IconTown class="instruction-icon" /> будут стоить
+                    <IconGold class="instruction-icon" /> по-разному.
                 </v-card-text>
                 <v-card-text>
-                    Одни и те же товары <IconSilk class="instruction-icon instruction-icon--silk" /> в разных городах <v-icon icon="mdi-home-city" :color="props.color" /> будут стоить
-                    <v-icon icon="mdi-label-percent-outline" :color="props.color" /> по-разному.
-                </v-card-text>
-                <v-card-text>
-                    Чтобы узнать специализацию города <v-icon icon="mdi-home-city" :color="props.color" />, читайте его описание <v-icon icon="mdi-script-text-outline" :color="props.color" />.
+                    Чтобы узнать специализацию города <IconTown class="instruction-icon" />, читайте его описание <v-icon icon="mdi-script-text-outline" :color="props.color" />
                 </v-card-text>
                 <v-card-text
                     >Чтобы выгодно торговать, вам надо покупать <v-icon icon="mdi-minus-circle-multiple" :color="props.color" /> товары <IconSilk class="instruction-icon instruction-icon--silk" /> в
-                    городах <v-icon icon="mdi-home-city" :color="props.color" />, где эти товары <IconSilk class="instruction-icon instruction-icon--silk" /> дешевле, и продавать
-                    <v-icon icon="mdi-plus-circle-multiple" :color="props.color" /> в городах <v-icon icon="mdi-home-city" :color="props.color" />, где товары дороже.</v-card-text
+                    городах <IconTown class="instruction-icon" />, где эти товары <IconSilk class="instruction-icon instruction-icon--silk" /> дешевле, и продавать
+                    <v-icon icon="mdi-plus-circle-multiple" :color="props.color" /> в городах <IconTown class="instruction-icon" />, где товары дороже.</v-card-text
                 >
-                <v-card-text>Также от города <v-icon icon="mdi-home-city" :color="props.color" /> зависит объем склада <v-icon icon="mdi-warehouse" :color="props.color" />. </v-card-text>
+                <v-card-text>Также от города <IconTown class="instruction-icon" /> зависит объем склада <v-icon icon="mdi-warehouse" :color="props.color" /> </v-card-text>
                 <v-card-text>
                     Это максимальный объем товаров
-                    <IconSilk class="instruction-icon instruction-icon--silk" />, который будет вмещать в себя город <v-icon icon="mdi-home-city" :color="props.color" />.
+                    <IconSilk class="instruction-icon instruction-icon--silk" />, который будет вмещать в себя город <IconTown class="instruction-icon" />
                 </v-card-text>
                 <v-card-text
                     >Если склад <v-icon icon="mdi-warehouse" :color="props.color" /> заполнен полностью, то постепенно количество товаров <IconSilk class="instruction-icon instruction-icon--silk" /> в
                     нем будет уменьшаться <v-icon icon="mdi-package-variant-closed-minus" :color="props.color" />, пока не достигнет половины объема склада
-                    <v-icon icon="mdi-warehouse" :color="props.color" />.</v-card-text
-                >
+                    <v-icon icon="mdi-warehouse" :color="props.color"
+                /></v-card-text>
                 <v-card-text
                     >Если склад <v-icon icon="mdi-warehouse" :color="props.color" /> заполнен менее чем наполовину, количество товаров будет расти
                     <v-icon icon="mdi-package-variant-closed-plus" :color="props.color" />, пока склад не заполнится полностью.</v-card-text
                 >
                 <v-card-text>
-                    В зависимости от объема склада <v-icon icon="mdi-warehouse" :color="props.color" /> цены <v-icon icon="mdi-label-percent-outline" :color="props.color" /> на товары
+                    В зависимости от объема склада <v-icon icon="mdi-warehouse" :color="props.color" /> цены <IconGold class="instruction-icon" /> на товары
                     <IconSilk class="instruction-icon instruction-icon--silk" /> будут меняться.
                 </v-card-text>
                 <v-card-text>
@@ -67,38 +65,29 @@ const props = defineProps<{
                     <IconSilk class="instruction-icon instruction-icon--silk" /> дороже.
                 </v-card-text>
                 <v-card-text>
-                    Если склад <v-icon icon="mdi-warehouse" :color="props.color" /> заполняется полностью, цены <v-icon icon="mdi-label-percent-outline" :color="props.color" /> на товар
+                    Если склад <v-icon icon="mdi-warehouse" :color="props.color" /> заполняется полностью, цены <IconGold class="instruction-icon" /> на товар
                     <IconSilk class="instruction-icon instruction-icon--silk" /> падают.
                 </v-card-text>
+                <v-card-text>Экран верфи <IconShipyard class="instruction-icon" /> предназначен для обслуживания ваших кораблей <IconShips class="instruction-icon" /> или покупки новых.</v-card-text>
                 <v-card-text
-                    >Экран верфи <IconShipyard class="instruction-icon" /> предназначен для обслуживания ваших кораблей <v-icon icon="mdi-airballoon" :color="props.color" /> или покупки
-                    новых.</v-card-text
-                >
-                <v-card-text
-                    >В разделе обслуживания корабля <v-icon icon="mdi-airballoon" :color="props.color" /> вы можете отремонтировать <IconArmor class="instruction-icon" /> и заправить
-                    <IconFuel class="instruction-icon" /> ваш корабль <v-icon icon="mdi-airballoon" :color="props.color" />.
+                    >В разделе обслуживания корабля <IconShips class="instruction-icon" /> вы можете отремонтировать <IconArmor class="instruction-icon" /> и заправить
+                    <IconFuel class="instruction-icon" /> ваш корабль <IconShips class="instruction-icon" />
                 </v-card-text>
                 <v-card-text>
                     Здесь же вы можете сменить корабль
-                    <v-icon icon="mdi-airballoon" :color="props.color" />.</v-card-text
-                >
+                    <IconShips class="instruction-icon"
+                /></v-card-text>
                 <v-card-text
-                    >Цена <v-icon icon="mdi-label-percent-outline" :color="props.color" /> ремонта зависит от корабля <v-icon icon="mdi-airballoon" :color="props.color" /> и его состояния
-                    <IconArmor class="instruction-icon" />.</v-card-text
-                >
+                    >Цена <IconGold class="instruction-icon" /> ремонта зависит от корабля <IconShips class="instruction-icon" /> и его состояния <IconArmor class="instruction-icon"
+                /></v-card-text>
+                <v-card-text> У более дорогих кораблей <IconShips class="instruction-icon" /> цена <IconGold class="instruction-icon" /> ремонта выше. </v-card-text>
+                <v-card-text>Чем хуже состояние <IconArmor class="instruction-icon" /> корабля <IconShips class="instruction-icon" />, тем дороже ремонт.</v-card-text>
+                <v-card-text>В разделе покупки корабля <IconBuy class="instruction-icon" /> вы можете приобрести новый корабль <IconShips class="instruction-icon" /></v-card-text>
                 <v-card-text>
-                    У более дорогих кораблей <v-icon icon="mdi-airballoon" :color="props.color" /> цена <v-icon icon="mdi-label-percent-outline" :color="props.color" /> ремонта выше.
+                    Более дорогие корабли <IconShips class="instruction-icon" /> имеют лучшие характеристики, в том числе и объем топлива <IconFuel class="instruction-icon" />, что позволит вам летать
+                    в более далекие города <IconTown class="instruction-icon" />
                 </v-card-text>
-                <v-card-text>Чем хуже состояние <IconArmor class="instruction-icon" /> корабля <v-icon icon="mdi-airballoon" :color="props.color" />, тем дороже ремонт.</v-card-text>
-                <v-card-text
-                    >В разделе покупки корабля <v-icon icon="mdi-airballoon" :color="props.color" /> вы можете приобрести новый корабль
-                    <v-icon icon="mdi-airballoon" :color="props.color" />.</v-card-text
-                >
-                <v-card-text>
-                    Более дорогие корабли <v-icon icon="mdi-airballoon" :color="props.color" /> имеют лучшие характеристики, в том числе и объем топлива <IconFuel class="instruction-icon" />, что
-                    позволит вам летать в более далекие города <v-icon icon="mdi-home-city" :color="props.color" />.
-                </v-card-text>
-                <v-card-text> Сражаться с пиратами <IconPirate class="instruction-icon" /> также проще на более мощных кораблях <v-icon icon="mdi-airballoon" :color="props.color" />. </v-card-text>
+                <v-card-text> Сражаться с пиратами <IconPirate class="instruction-icon" /> также проще на более мощных кораблях <IconShips class="instruction-icon" /> </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
                     <v-btn :color="props.color" text="Закрыть" @click="isActive.value = false"></v-btn>
