8987957ef537d4dc8282e1544edb3fd84515372a
Павел Бездорнов <alpinweb@yandex.ru>
2024-08-16 10:32:30 +0600

    update MapInstruction

--- src/ui-components/MapInstruction.component.vue
+++ src/ui-components/MapInstruction.component.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import IconGold from "/public/assets/icons/resources/gold.svg"
+import IconFuel from "/public/assets/icons/ship-params/fuel.svg"
+import IconArmor from "/public/assets/icons/ship-params/shield.svg"
+import IconSink from "/public/assets/icons/alerts/sink.svg"
+import IconPirate from "/public/assets/icons/alerts/pirate.svg"
 
 const props = defineProps<{
     color: string
@@ -13,16 +17,36 @@ const props = defineProps<{
         </template>
         <template #default="{ isActive }">
             <v-card title="Инструкции во время полетов" :color="`${props.color}-lighten-5`">
-                <v-card-text>Для передвижения по небу нажмите левой кнопкой мыши на место, куда вы хотите полететь, и ваш корабль начнет движение в этом направлении.</v-card-text>
-                <v-card-text>Для ориентации используйте карту, которую можно вызвать в нижнем левом углу.</v-card-text>
-                <v-card-text>Во время передвижения ваш корабль тратит топливо. Также состояние корабля медленно ухудшается.</v-card-text>
-                <v-card-text>Если хотя бы один показатель корабля опустится до нуля, ваш корабль потерпит крушение. В этом случае вы потеряете корабль и весь ваш груз.</v-card-text>
-                <v-card-text>Золото <IconGold class="gold-icon" /> при крушении не теряется.</v-card-text>
-                <v-card-text>Ваш груз показан на панели сверху.</v-card-text>
-                <v-card-text>В некоторых районах неба на вас могут напасть пираты. Цель пиратов — уничтожить ваш корабль и захватить груз.</v-card-text>
-                <v-card-text>Чтобы скрыться от пиратов, вы должны приземлиться в любом из городов.</v-card-text>
-                <v-card-text>Вы можете сразиться с пиратами. Для того чтобы стрелять по пиратам, нажмите клавишу "ПРОБЕЛ".</v-card-text>
-                <v-card-text class="instruction">За уничтожение каждого пиратского корабля вы получаете награду в 10000 <IconGold class="gold-icon" /></v-card-text>
+                <v-card-text>
+                    Для передвижения по небу нажмите левой кнопкой мыши <v-icon icon="mdi-mouse-left-click" :color="props.color" /> на место, куда вы хотите полететь, и ваш корабль
+                    <v-icon icon="mdi-airballoon" :color="props.color" /> начнет движение <v-icon icon="mdi-ship-wheel" :color="props.color" /> в этом направлении.
+                </v-card-text>
+                <v-card-text>Для ориентации используйте карту <v-icon icon="mdi-map" :color="props.color" />, которую можно вызвать в нижнем левом углу.</v-card-text>
+                <v-card-text
+                    >Во время передвижения <v-icon icon="mdi-ship-wheel" :color="props.color" /> ваш корабль <v-icon icon="mdi-airballoon" :color="props.color" /> тратит топливо
+                    <IconFuel class="instruction-icon" />. Также состояние <IconArmor class="instruction-icon" /> корабля медленно ухудшается.</v-card-text
+                >
+                <v-card-text
+                    >Если хотя бы один показатель корабля <IconArmor class="instruction-icon" /> <IconFuel class="instruction-icon" /> опустится до нуля, ваш корабль потерпит крушение
+                    <IconSink class="instruction-icon" />. В этом случае вы потеряете корабль <v-icon icon="mdi-airballoon" :color="props.color" /> и весь ваш груз
+                    <v-icon icon="mdi-train-car-container" :color="props.color" />.</v-card-text
+                >
+                <v-card-text>Золото <IconGold class="instruction-icon" /> при крушении <IconSink class="instruction-icon" /> не теряется.</v-card-text>
+                <v-card-text>Ваш груз <v-icon icon="mdi-train-car-container" :color="props.color" /> показан на панели сверху.</v-card-text>
+                <v-card-text
+                    >В некоторых районах неба на вас могут напасть пираты <IconPirate class="instruction-icon" />. Цель пиратов <IconPirate class="instruction-icon" /> — уничтожить ваш корабль
+                    <v-icon icon="mdi-airballoon" :color="props.color" /> и захватить груз <v-icon icon="mdi-train-car-container" :color="props.color" />.</v-card-text
+                >
+                <v-card-text
+                    >Чтобы скрыться от пиратов <IconPirate class="instruction-icon" />, вы должны приземлиться в любом из городов <v-icon icon="mdi-home-city" :color="props.color" />.</v-card-text
+                >
+                <v-card-text
+                    >Вы можете сразиться с пиратами <IconPirate class="instruction-icon" />. Для того чтобы стрелять по пиратам, нажмите клавишу "ПРОБЕЛ"
+                    <v-icon icon="mdi-keyboard-space" :color="props.color" />.</v-card-text
+                >
+                <v-card-text class="instruction"
+                    >За уничтожение каждого пиратского корабля <IconPirate class="instruction-icon" /> вы получаете награду в 10000 <IconGold class="instruction-icon"
+                /></v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
                     <v-btn :color="props.color" text="Закрыть" @click="isActive.value = false"></v-btn>
@@ -35,7 +59,7 @@ const props = defineProps<{
 <style scoped lang="scss">
 @import "/public/assets/scss/mixins.scss";
 
-.gold-icon {
+.instruction-icon {
     @include icon-styles;
     position: relative;
     top: 4px;
