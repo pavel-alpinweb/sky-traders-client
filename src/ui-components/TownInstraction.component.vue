f476bc4eaba524d9df570144cd98c842f8a57c02
Павел Бездорнов <alpinweb@yandex.ru>
2024-08-21 10:19:22 +0600

    fix map instructions

--- src/ui-components/TownInstraction.component.vue
+++ src/ui-components/TownInstraction.component.vue
@@ -16,7 +16,7 @@ const props = defineProps<{
 </script>
 
 <template>
-    <v-dialog max-width="600" max-height="768">
+    <v-dialog max-width="600" max-height="90vh">
         <template #activator="{ props: activatorProps }">
             <v-btn v-bind="activatorProps" :color="`${props.color}-darken-4`" variant="elevated" size="x-large" v-tooltip="'Инструкция'" icon="mdi-help-circle" />
         </template>
